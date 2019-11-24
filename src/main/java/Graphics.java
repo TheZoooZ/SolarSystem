503e4b2918291dd0d16ab6847000767ac4c4a1e8
Tomasz Falarz <t.k.falarz@gmail.com>
2019-11-24 21:17:09 +0100

    Animation of Earth added.

--- src/main/java/Graphics.java
+++ src/main/java/Graphics.java
@@ -4,36 +4,24 @@ import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 
 public class Graphics {
+    private int angle = 0;
+
     public static void drawCelestialObject(GL2 gl, CelestialObject object) {
         gl.glColor3f(object.Colour.Red, object.Colour.Green, object.Colour.Blue);
         gl.glBegin(GL2.GL_POLYGON);
         drawPolygon(gl, object);
+        gl.glEnd();
     }
 
     public static void drawBackground(GLAutoDrawable glAutoDrawable) {
         GL2[] dots = new GL2[Constants.NumberOfPoints];
         for (int i = 0; i < Constants.NumberOfPoints; i++) {
             dots[i] = glAutoDrawable.getGL().getGL2();
-            dots[i].glColor3f(255,250,250);
+            dots[i].glColor3f(255, 250, 250);
             dots[i].glBegin(GL2.GL_POINTS);
             dots[i].glVertex2d(Constants.coords[i].X, Constants.coords[i].Y);
             dots[i].glEnd();
         }
-    }
-
-    public static void drawOrbit(GLAutoDrawable glAutoDrawable, Coords centralPoint, int range) {
-        GL2 circle = glAutoDrawable.getGL().getGL2();
-        circle.glBegin(GL2.GL_LINES);
-        double angle;
-        int numVertices = 100;
-        double angleIncrement = 2 * Math.PI / numVertices;
-        for (int i = 0; i < numVertices; i++) {
-            angle = i * angleIncrement;
-            double x = centralPoint.X + (range * Math.cos(angle));
-            double y = centralPoint.Y + (range * Math.sin(angle));
-            circle.glVertex2d(x, y);
-        }
-        circle.glEnd();
     }
 
     private static void drawPolygon(GL2 gl, CelestialObject object) {
@@ -46,6 +34,5 @@ public class Graphics {
             double y = object.Coords.Y + (object.Diameter / 2) * Math.sin(angle);
             gl.glVertex2f((float) x, (float) y);
         }
-        gl.glEnd();
     }
 }
--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -8,9 +8,10 @@ import com.jogamp.opengl.GLEventListener;
 
 public class Listener implements GLEventListener {
     private static Coords sunStartCoords = new Coords(0, 0);
-    private static Coords earthStartCoords = new Coords(sunStartCoords.X + 200, sunStartCoords.Y + 0);
+    private static Coords earthStartCoords = new Coords(200, 0);
     private CelestialObject sun = new Sun(sunStartCoords, 0, 0, 0);
     private CelestialObject earth = new Earth(earthStartCoords, 0, 0, 0);
+    private double angle = 0;
     private GL2 glSun;
     private GL2 glEarth;
 
@@ -24,9 +25,12 @@ public class Listener implements GLEventListener {
     }
 
     public void display(GLAutoDrawable glAutoDrawable) {
+        glEarth.glClear(GL2.GL_COLOR_BUFFER_BIT);
+        glEarth.glLoadIdentity();
         Graphics.drawBackground(glAutoDrawable);
         Graphics.drawCelestialObject(glSun, sun);
-        Graphics.drawCelestialObject(glEarth,earth);
+        Graphics.drawCelestialObject(glEarth, earth);
+        animate(earth);
     }
 
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
@@ -41,5 +45,12 @@ public class Listener implements GLEventListener {
                 -1,
                 1);
         gl.glMatrixMode(GL2.GL_MODELVIEW);
+    }
+
+    private void animate(CelestialObject object) {
+        double range = 300;
+        object.Coords.X = (int) (range * Math.cos(angle));
+        object.Coords.Y = (int) (range * Math.sin(angle));
+        angle = (angle + 0.01) % 360;
     }
 }
